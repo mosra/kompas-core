a518505f5ee7f99a7764431668688986e847560e
Vladimír Vondruš <mosra@centrum.cz>
2011-02-17 20:10:34 +0100

    Module for autodetecting LIB_SUFFIX, if not present.

--- modules/FindKompasCore.cmake
+++ modules/FindKompasCore.cmake
@@ -54,6 +54,7 @@ endif()
 
 if(KOMPASCORE_FOUND)
     include(KompasMacros)
+    include(KompasLibSuffix)
 
     # Installation dirs
     if(WIN32)
@@ -64,7 +65,7 @@ if(KOMPASCORE_FOUND)
         set_parent_scope(KOMPAS_DATA_INSTALL_DIR )
     else()
         set_parent_scope(KOMPAS_BINARY_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/bin)
-        set_parent_scope(KOMPAS_LIBRARY_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/lib)
+        set_parent_scope(KOMPAS_LIBRARY_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/lib${LIB_SUFFIX})
         set_parent_scope(KOMPAS_CMAKE_MODULE_INSTALL_DIR ${CMAKE_ROOT}/Modules)
         set_parent_scope(KOMPAS_INCLUDE_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/include/Kompas)
         set_parent_scope(KOMPAS_DATA_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/share/kompas)
