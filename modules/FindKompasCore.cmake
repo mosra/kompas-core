mosra/kompas-core

commit 5383996b017a330aa155311a36a75f1c788988e8

Install plugins into /usr/lib instead of /usr/share.
According to filesystem hierarchy specifications, into /usr/share
belongs only architecture-independent files. Spotted with rpmlint.

modules/FindKompasCore.cmake
@@ -79,7 +79,7 @@ if(KOMPASCORE_FOUND)
 
     set_parent_scope(KOMPAS_CORE_INCLUDE_INSTALL_DIR ${KOMPAS_INCLUDE_INSTALL_DIR}/Core)
     set_parent_scope(KOMPAS_PLUGINS_INCLUDE_INSTALL_DIR ${KOMPAS_INCLUDE_INSTALL_DIR}/Plugins)
-    set_parent_scope(KOMPAS_PLUGINS_INSTALL_DIR ${KOMPAS_DATA_INSTALL_DIR}/plugins)
+    set_parent_scope(KOMPAS_PLUGINS_INSTALL_DIR ${KOMPAS_LIBRARY_INSTALL_DIR}/kompas)
     set_parent_scope(KOMPAS_PLUGINS_PROJECTION_INSTALL_DIR ${KOMPAS_PLUGINS_INSTALL_DIR}/projections)
     set_parent_scope(KOMPAS_PLUGINS_RASTERMODEL_INSTALL_DIR ${KOMPAS_PLUGINS_INSTALL_DIR}/rasterModels)
 endif()

commit 4226e603667fd6492ccf67958da9b238c70b436a

Install plugins on Win32 to plugins/ dir, like it was before.
Fixes problem introduced in 5383996b017a330aa155311a36a75f1c788988e8.

modules/FindKompasCore.cmake
@@ -65,11 +65,13 @@ if(KOMPASCORE_FOUND)
         set_parent_scope(KOMPAS_LIBRARY_INSTALL_DIR .)
         set_parent_scope(KOMPAS_CMAKE_MODULE_INSTALL_DIR Modules)
         set_parent_scope(KOMPAS_INCLUDE_INSTALL_DIR include/Kompas)
+        set_parent_scope(KOMPAS_PLUGINS_INSTALL_DIR /plugins)
         set_parent_scope(KOMPAS_DATA_INSTALL_DIR )
     else()
         include(KompasLibSuffix)
         set_parent_scope(KOMPAS_BINARY_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/bin)
         set_parent_scope(KOMPAS_LIBRARY_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/lib${LIB_SUFFIX})
+        set_parent_scope(KOMPAS_PLUGINS_INSTALL_DIR ${KOMPAS_LIBRARY_INSTALL_DIR}/kompas)
         set_parent_scope(KOMPAS_CMAKE_MODULE_INSTALL_DIR ${CMAKE_ROOT}/Modules)
         set_parent_scope(KOMPAS_INCLUDE_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/include/Kompas)
         set_parent_scope(KOMPAS_DATA_INSTALL_DIR ${CMAKE_INSTALL_PREFIX}/share/kompas)
@@ -79,7 +81,6 @@ if(KOMPASCORE_FOUND)
 
     set_parent_scope(KOMPAS_CORE_INCLUDE_INSTALL_DIR ${KOMPAS_INCLUDE_INSTALL_DIR}/Core)
     set_parent_scope(KOMPAS_PLUGINS_INCLUDE_INSTALL_DIR ${KOMPAS_INCLUDE_INSTALL_DIR}/Plugins)
-    set_parent_scope(KOMPAS_PLUGINS_INSTALL_DIR ${KOMPAS_LIBRARY_INSTALL_DIR}/kompas)
     set_parent_scope(KOMPAS_PLUGINS_PROJECTION_INSTALL_DIR ${KOMPAS_PLUGINS_INSTALL_DIR}/projections)
     set_parent_scope(KOMPAS_PLUGINS_RASTERMODEL_INSTALL_DIR ${KOMPAS_PLUGINS_INSTALL_DIR}/rasterModels)
 endif()