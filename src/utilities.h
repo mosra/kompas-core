f3732ed9357d6f3de3203aa08dec5554182def7f
Vladimír Vondruš <mosra@centrum.cz>
2012-04-12 17:32:34 +0200

    Temporary fix for building with what will eventually become Corrade 0.3.

--- src/utilities.h
+++ src/utilities.h
@@ -29,4 +29,22 @@
     #define CORE_EXPORT
 #endif
 
+/* Temporary fix for building with what will eventually become Corrade 0.3 */
+#include "PluginManager/Plugin.h"
+
+#ifndef PLUGIN_REGISTER_STATIC
+#undef PLUGIN_REGISTER
+#define PLUGIN_REGISTER_STATIC(name, className, interface)                  \
+    inline void* pluginInstancer_##name(Corrade::PluginManager::AbstractPluginManager* manager, const std::string& plugin) \
+        { return new className(manager, plugin); }                          \
+    int pluginInitializer_##name() {                                        \
+        Corrade::PluginManager::AbstractPluginManager::importStaticPlugin(#name, PLUGIN_VERSION, interface, pluginInstancer_##name); return 1; \
+    } AUTOMATIC_INITIALIZER(pluginInitializer_##name)
+#define PLUGIN_REGISTER(className, interface)                               \
+    extern "C" int pluginVersion() { return PLUGIN_VERSION; }               \
+    extern "C" void* pluginInstancer(Corrade::PluginManager::AbstractPluginManager* manager, const std::string& plugin) \
+        { return new className(manager, plugin); }                          \
+    extern "C" std::string pluginInterface() { return interface; }
+#endif
+
 #endif
